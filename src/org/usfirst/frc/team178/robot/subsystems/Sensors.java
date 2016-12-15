89dfc7dcda6250edf9314826ddfb01a4a37a1c55
poojpooj <pswami1600@gmail.com>
2016-12-15 18:22:55 -0500

    did stuff, yay

---
+++ src/org/usfirst/frc/team178/robot/subsystems/Sensors.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team178.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Sensors extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	Ultrasonic Sensor;
+	DigitalInput Bumper;
+	
+	public Sensors()  {
+		
+		Bumper = new DigitalInput(0);
+		Sensor = new Ultrasonic(9,9);
+		
+	}
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+  
+    }
+    public boolean getBumperValue() {
+    	return Bumper.get(); 
+    }
+    
+    
+    public double getDistance() {
+    	return Sensor.pidGet();
+    }
+    
+    
+    
+}
+
